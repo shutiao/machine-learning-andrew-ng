10461920f27927ed5ce7f11ba257141f729ef9e3
Haihua Xu <xuhaihua90@gmail.com>
2022-01-27 21:19:56 +0800

    Part 3: Compute Cost

---
+++ ex1-py/computeCost.py
@@ -0,0 +1,11 @@
+# COMPUTECOST Compute cost for linear regression
+# RETRUN J = COMPUTECOST(X, y, theta) computes the cost of using theta as the 
+# parameter for linea regression to fit the data points in X and y
+
+import numpy as np
+import logging
+
+def compute_cost(X, y, theta):
+    inner = np.power(((X * theta.T) - y), 2)
+    print('inner.shape', inner.shape)
+    return np.sum(inner) / (2 * len(X))
--- ex1-py/ex1.py
+++ ex1-py/ex1.py
@@ -24,3 +24,28 @@ path = 'ex1data1.txt'
 data = pd.read_csv(path, names = ['population', 'profit'])
 plotData.plot_data(data)
 
+print('# =================== Part 3: Cost and Gradient descent ===================')
+
+data.insert(0, 'intercept', 1)  # Add a column of ones to x
+m = data.shape[0] # number of training examples
+n = data.shape[1] # number of columns
+X = data.iloc[:, 0:n-1 ]
+y = data.iloc[:, n-1: n]
+
+print('check whether X 是所有行，去掉最后一列')
+print(X.head())
+print('check whether y 是所有行，只含最后一列')
+print(y.head())
+
+X = np.matrix(X.values) # initialize X and y
+y = np.matrix(y.values)
+theta = np.matrix(np.array([0,0])) # initialize fitting parameters
+
+print('check if matrix([[0, 0]]), theta 是一个(1,2)矩阵')
+print(theta)
+
+print('X.shape', X.shape)
+print('theta.shape', theta.shape)
+print('y.shape', y.shape)
+
+print('With theta = [0,0], Cost computed is', computeCost.compute_cost(X, y, theta), 'Expected cost value (approx) 32.07')
